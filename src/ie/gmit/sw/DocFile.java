a00ebe737bafdfd41357b1c79416261a9db637eb
DuffyTJ89 <tomduffy89@gmail.com>
2018-01-09 21:13:33 +0000

    added docfile and shingle class

---
+++ src/ie/gmit/sw/DocFile.java
@@ -0,0 +1,23 @@
+package ie.gmit.sw;
+
+public class DocFile implements Document{
+	
+	String path;
+	String text;
+	
+	
+	
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/ie/gmit/sw/Shingle.java
@@ -0,0 +1,23 @@
+package ie.gmit.sw;
+
+public class Shingle {
+
+	private int docId;
+	private int hashcode;
+	
+	
+	public int getDocId() {
+		return docId;
+	}
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+	public int getHashcode() {
+		return hashcode;
+	}
+	public void setHashcode(int hashcode) {
+		this.hashcode = hashcode;
+	}
+	
+//	
+}
